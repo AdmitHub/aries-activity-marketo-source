c9290f245a96919c88034122bbab759bbe271044
Noah Bass <noah@astronomer.io>
2017-06-09 16:28:53 -0400

    Update tests

---
+++ test/fixtures/index.js
@@ -0,0 +1,4 @@
+export { default as listDescribeLeads } from './listDescribeLeads.json';
+export { default as lookupLeads } from './lookupLeads.json';
+export { default as getStaticList } from './getStaticList.json';
+export { default as oauth } from './oauth.json';
--- test/test.js
+++ test/test.js
@@ -1,58 +1,83 @@
 import { assert } from 'chai';
-import MarketoSource from '../lib/index.js';
-import config from './test-config.js';
-// import nock from 'nock';
-// import * as fixtures from './fixtures';
-
-const options = {
-    clientId: config.clientId,
-    clientSecret: config.clientSecret,
-    endpoint: config.endpoint,
-    identity: config.identity,
-};
+import nock from 'nock';
+import * as fixtures from './fixtures';
+import config from './test-config';
+import MarketoSource from '../lib/index';
 
 let source = null;
 
 describe('MarketoSource', () => {
-    before(async () => {
-        // grab an access token for tests
-        source = new MarketoSource(options.clientId, options.clientSecret,
-                                   options.endpoint, options.identity);
-        await source.auth();
+    afterEach(async () => {
+        // clear out nock interceptor(s) after each test
+        nock.cleanAll();
     });
 
     describe('#auth', () => {
+        before(async () => {
+            nock(config.endpoint)
+                .get('/oauth/token?grant_type=client_credentials')
+                .query({ client_id: config.clientId, client_secret: config.clientSecret })
+                // return a fake access token
+                .reply(200, fixtures.oauth);
+        });
+
         it('should grab an access token', async () => {
-            const s = new MarketoSource(options.clientId, options.clientSecret,
-                                        options.endpoint, options.identity);
-            await s.auth();
-            assert.isOk(s.accessToken);
+            source = new MarketoSource(config.clientId, config.clientSecret,
+                                       config.endpoint, config.identity);
+            await source.auth();
+
+            assert.isOk(source.accessToken);
         });
     });
 
-    describe('#listDescribeLeads', () => {
-        it('should filter based on fieldKey and fieldValue', async () => {
-            const result = await source.listDescribeLeads({ fieldKey: 'displayName', fieldValue: 'Site' });
-            assert.isOk(result);
-        });
-    });
-
-    describe('#lookupLeads', () => {
-        it('should return ids corresponding to fieldKey and fieldValue', async () => {
-            const result = await source.lookupLeads({ fieldKey: 'displayName', fieldValue: 'Site' });
-            assert.isOk(result);
-            assert.isTrue(result instanceof Array);
-        });
-    });
-
-    describe('#getStaticList', () => {
-        it('should return data from a static list', async () => {
-            const result = await source.getStaticList({
-                listId: '57777',
-                fields: 'firstName,lastName,id,company',
-            });
-
-            assert.isOk(result);
-        });
-    });
+    // describe('#listDescribeLeads', () => {
+    //     before(async () => {
+    //         nock(ENDPOINT_URI)
+    //             .get('/v1/leads/describe.json')
+    //             .query({ access_token: source.accessToken })
+    //             .reply(200, fixtures.listDescribeLeads);
+    //     });
+    //
+    //     it('should filter based on fieldKey and fieldValue', async () => {
+    //         const result = await source.listDescribeLeads({ fieldKey: 'displayName', fieldValue: 'Marketo Social Twitter Reach' });
+    //
+    //         assert.isOk(result);
+    //     });
+    // });
+    //
+    // describe('#lookupLeads', () => {
+    //     before(async () => {
+    //         nock(ENDPOINT_URI)
+    //             .get('/v1/leads/describe.json')
+    //             .query({ access_token: source.accessToken })
+    //             .reply(200, fixtures.listDescribeLeads);
+    //         nock(ENDPOINT_URI)
+    //             .get('/v1/leads.json')
+    //             // note: `3` is the response from listDescribeLeads
+    //             .query({ access_token: source.accessToken, filterType: 'id', filterValues: '3' })
+    //             .reply(200, fixtures.lookupLeads);
+    //     });
+    //
+    //     it('should return ids corresponding to fieldKey and fieldValue', async () => {
+    //         const result = await source.lookupLeads({ fieldKey: 'displayName', fieldValue: 'Marketo Social Twitter Reach' });
+    //
+    //         assert.isOk(result);
+    //     });
+    // });
+    //
+    // describe('#getStaticList', () => {
+    //     before(async () => {
+    //         nock(ENDPOINT_URI)
+    //             // note: `57777` is listId
+    //             .get('/v1/lists/57777/leads.json')
+    //             .query({ access_token: source.accessToken, fields: 'firstName,lastName,id,company' })
+    //             .reply(200, fixtures.getStaticList);
+    //     });
+    //
+    //     it('should return data from a static list', async () => {
+    //         const result = await source.getStaticList({ listId: '57777', fields: 'firstName,lastName,id,company' });
+    //
+    //         assert.isOk(result);
+    //     });
+    // });
 });
